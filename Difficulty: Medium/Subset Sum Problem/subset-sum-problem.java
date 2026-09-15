aed9b74cad2c09d2562baf1f2ae8a4dcdad10a24
Anjali Sevkani <152064397+anjalisevkani@users.noreply.github.com>
2026-09-15 16:14:10 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Subset Sum Problem/subset-sum-problem.java
@@ -0,0 +1,25 @@
+class Solution {
+    static boolean isSubsetSum(int arr[], int sum) {
+        // code here
+        int n=arr.length;
+        boolean[][] dp=new boolean[n][sum+1];
+        for(int i=0;i<n;i++){
+            dp[i][0]=true;
+        }
+        if(arr[0]<=sum){
+            dp[0][arr[0]]=true;
+        }
+        for(int i=1;i<n;i++){
+            for(int j=1;j<=sum;j++){
+                boolean notTake=dp[i-1][j];
+                boolean take=false;
+                if(arr[i]<=j){
+                    take=dp[i-1][j-arr[i]];
+                }
+                dp[i][j]=take||notTake;
+            }
+        }
+        return dp[n-1][sum];
+        
+    }
+}
