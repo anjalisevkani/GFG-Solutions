a97c18e9bfab1826e2dafe1ead18dc1a3088027e
Anjali Sevkani <152064397+anjalisevkani@users.noreply.github.com>
2026-09-22 16:30:40 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Partition Equal Subset Sum/partition-equal-subset-sum.java
+++ Difficulty: Medium/Partition Equal Subset Sum/partition-equal-subset-sum.java
@@ -1,40 +1,31 @@
 class Solution {
-    static boolean equalPartition(int[] arr) {
-        int n = arr.length;
-
-        int total = 0;
-        for (int x : arr) {
-            total += x;
+    static boolean equalPartition(int[] nums) {
+        int n=nums.length;
+        int totalSum=0;
+        for(int i=0;i<n;i++){
+            totalSum+=nums[i];
         }
-
-        // Odd total cannot be divided equally
-        if (total % 2 != 0) {
+        if(totalSum%2==1){
             return false;
         }
-
-        int target = total / 2;
-
-        boolean[][] dp = new boolean[n + 1][target + 1];
-
-        // Sum 0 is always possible: choose nothing
-        for (int i = 0; i <= n; i++) {
-            dp[i][0] = true;
+        return subsetSum(nums,totalSum/2);
+    }
+    static boolean subsetSum(int[] nums,int sum){
+        int n=nums.length;
+        boolean[][] dp=new boolean[n+1][sum+1];
+        for(int j=0;j<=sum;j++){
+            dp[n][j]=false;
         }
-
-        for (int i = 1; i <= n; i++) {
-            for (int sum = 1; sum <= target; sum++) {
-
-                // Don't take arr[i-1]
-                dp[i][sum] = dp[i - 1][sum];
-
-                // Take arr[i-1] if possible
-                if (arr[i - 1] <= sum) {
-                    dp[i][sum] = dp[i][sum] ||
-                                 dp[i - 1][sum - arr[i - 1]];
+        dp[n][0]=true;
+        for(int i=n-1;i>=0;i--){
+            for(int j=0;j<=sum;j++){
+                if(nums[i]>j){
+                    dp[i][j]=dp[i+1][j];
+                }else{
+                    dp[i][j]=dp[i+1][j-nums[i]] || dp[i+1][j];
                 }
             }
         }
-
-        return dp[n][target];
+        return dp[0][sum];
     }
 }
