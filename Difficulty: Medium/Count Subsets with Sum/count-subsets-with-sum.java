ccc82e55bced601713007d6c6b291aa5ed8de9cd
Anjali Sevkani <152064397+anjalisevkani@users.noreply.github.com>
2026-09-22 15:49:45 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Count Subsets with Sum/count-subsets-with-sum.java
+++ Difficulty: Medium/Count Subsets with Sum/count-subsets-with-sum.java
@@ -1,27 +1,18 @@
 class Solution {
     static int perfectSum(int[] arr, int target) {
-        int n = arr.length;
-
-        int[][] dp = new int[n + 1][target + 1];
-
-        // One subset has sum 0: the empty subset
-        for (int i = 0; i <= n; i++) {
-            dp[i][0] = 1;
-        }
-
-        for (int i = 1; i <= n; i++) {
-            for (int sum = 0; sum <= target; sum++) {
-
-                // Don't take current element
-                dp[i][sum] = dp[i - 1][sum];
-
-                // Take current element
-                if (arr[i - 1] <= sum) {
-                    dp[i][sum] += dp[i - 1][sum - arr[i - 1]];
+        int n=arr.length;
+        int[][] dp=new int[n+1][target+1];
+        
+        dp[n][0]=1;
+        for(int i=n-1;i>=0;i--){
+            for(int j=0;j<=target;j++){
+                if(arr[i]>j){
+                    dp[i][j]=dp[i+1][j];
+                }else{
+                    dp[i][j]=dp[i+1][j-arr[i]]+dp[i+1][j];
                 }
             }
         }
-
-        return dp[n][target];
+        return dp[0][target];
     }
 }
